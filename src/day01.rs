f08ab065dbd78cabd756d9bab7f3cbe425ac22aa
Sarah Trefethen <sarahtrefethen@noreply.github.com>
2024-12-01 21:32:36 -0500

    day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, collections::HashMap};
 
 pub(crate) fn day01 () {
     let path = "input/D01.txt";
@@ -22,5 +22,29 @@ pub(crate) fn day01 () {
     let diffs: Vec<i32> = left_column.iter().map(|x| (x-right_iter.next().unwrap()).abs()).collect();
 
     println!("Part 1: {:?}", diffs.iter().sum::<i32>());
+
+    //////// Part Two
+    let mut total: i32 = 0;
+    right_iter = right_column.iter();
+    let mut left_iter = left_column.iter();
+    
+    // count how many times each number appears in each list
+    let mut right_map: HashMap<i32, i32> = HashMap::new();
+    let mut left_map: HashMap<i32, i32> = HashMap::new();
+    while let Some(left) = left_iter.next() {
+        let total = left_map.entry(left.clone()).or_insert(0);
+        *total += 1;
+    }
+    while let Some(right) = right_iter.next() {
+        let total = right_map.entry(right.clone()).or_insert(0);
+        *total += 1;
+    } 
+
+    for (key,count) in left_map {
+        total += key*count**right_map.entry(key).or_default();
+    }
+
+
+    println!("part 2: {}", total)
 
 }
