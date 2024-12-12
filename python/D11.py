286d15f86e8896856b07c205ef639d645be820c0
Sarah Trefethen <sarahtrefethen@noreply.github.com>
2024-12-11 23:09:39 -0500

    day 11 part 2

--- python/D11.py
+++ python/D11.py
@@ -17,9 +17,39 @@ def part1(input):
         stones = list(blink(stones))
     return len(stones)
 
+memo25 = {}
+
+def blink25(stone):
+    if stone in memo25:
+        return memo25[stone]
+    s = [stone]
+    for i in range(0,25):
+        s = list(blink(s))  
+    memo25[stone] = s
+    return s
+
+def blink75(stone):
+    print(stone)
+    twentyfive = blink25(stone)
+    fifty = []
+    for stone in twentyfive:
+        fifty.extend(blink25(stone))
+    total = 0
+    for stone in fifty:
+        total += len(blink25(stone))
+    return total
+
+def part2(input):
+    stones = [n for n in input.strip().split(" ")]
+    total = 0
+    for stone in stones:
+        total += blink75(stone)
+    return total
+
 assert part1("125 17") == 55312
 
 with open('../input/D11.txt', 'r') as file:
     input = file.read()
 
 print(f'part 1: {part1(input)}')
+print(f'part 2: {part2(input)}')
